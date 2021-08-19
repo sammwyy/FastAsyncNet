bdd0d9f9dc7cc315e60278f24a7916ca0764d7b1
Sammwy <sammwy.dev@gmail.com>
2021-08-19 00:26:00 -0300

    Better example

--- FastAsyncNet.Examples/src/Program.cs
+++ FastAsyncNet.Examples/src/Program.cs
@@ -8,8 +8,17 @@ namespace example
         static void Main(string[] args)
         {
             TcpServer server = new TcpServer(8080, "127.0.0.1");
-            Console.WriteLine("1Test");
+            server.SetHandler(new Handler());
             server.Listen();
+            Console.WriteLine("Started Server");
+        }
+    }
+
+    class Handler : ServerHandler
+    {
+        public override void Handle(Connection connection, byte[] data)
+        {
+            connection.Send("HTTP/1.1 200 OK\nContent-Length: 11\n\nHello World");
         }
     }
 }
